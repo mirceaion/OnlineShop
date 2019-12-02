8ca25f37d9627fd6ecc5c84d403bd5f5d6ffa7ce
Mircea Ion <mircea_ion@hotmail.com>
2019-12-02 11:43:59 -0500

    Initial commit, a database project, a webapp and a .Net core WebApi repository

---
+++ Database/Tables/Customers.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Customers]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(50) NULL, 
+    [Email] VARCHAR(100) NOT NULL, 
+    [Password] NCHAR(30) NOT NULL
+)
+
+GO
+
+CREATE INDEX [IX_Customers_Column] ON [dbo].[Customers] ([Name])
---
+++ Database/Tables/Purchases.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Purchases]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [UserId] INT NOT NULL, 
+    [ProductId] INT NOT NULL, 
+    CONSTRAINT [FK_Purchases_ToCustomers] FOREIGN KEY ([UserId]) REFERENCES [Customers]([Id]), 
+    CONSTRAINT [FK_Purchases_ToProducts] FOREIGN KEY ([ProductId]) REFERENCES [Products]([Id]) 
+	
+)
+
+GO
+
+CREATE INDEX [IX_Purchases_UserId] ON [dbo].[Purchases] ([UserId])
+
+GO
+
+CREATE INDEX [IX_Purchases_ProductId] ON [dbo].[Purchases] ([ProductId])
